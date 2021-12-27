24e95ee824bf6be72881a19d4e0beacc1834ffb9
Fernando Guerra <fernandoguerra@iMacdeFernando.spaceworks.cl>
2021-12-27 12:16:57 -0300

    updatecabecera

--- src/layout/MainLayout/Header/index.js
+++ src/layout/MainLayout/Header/index.js
@@ -58,17 +58,17 @@ const Header = ({ handleLeftDrawerToggle }) => {
             </Box>
 
             {/* header search */}
-            <SearchSection />
+           { /* <SearchSection />  */ }
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ flexGrow: 1 }} />
 
-            {/* live customization & localization */}
+            {/* live customization & localization 
             <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                 <LocalizationSection />
-            </Box>
+            </Box> */ }
 
             {/* notification & profile */}
-            <NotificationSection />
+            {/*<NotificationSection /> */}
             <ProfileSection />
 
             {/* mobile header */}
